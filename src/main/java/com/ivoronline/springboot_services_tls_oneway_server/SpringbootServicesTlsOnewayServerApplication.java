99dbd53aef2eaf8252a060e86f892f7c0efbae5a
igoreta <ivoronline@gmail.com>
2021-06-19 22:41:18 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_services_tls_oneway_server/SpringbootServicesTlsOnewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_services_tls_oneway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicesTlsOnewayServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootServicesTlsOnewayServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_services_tls_oneway_server/SpringbootServicesTlsOnewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_services_tls_oneway_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicesTlsOnewayServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
